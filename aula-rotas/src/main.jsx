76430e053ee571d66489af141c78b62155984d36
gallone10 <enzo.gallone83@gmail.com>
2023-09-13 08:33:19 -0300

    createBrowserRouter criado

--- aula-rotas/src/main.jsx
+++ aula-rotas/src/main.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Error from './routes/Error/index.jsx'
+import EditarProdutos from './routes/EditarProdutos/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+
+const router = createBrowserRouter([
+  { 
+
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/produtos",
+        element: <Produtos/>
+      },
+      {
+        path: "/produtos/editar/:id",
+        element: <EditarProdutos/>
+      }
+    ]
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
