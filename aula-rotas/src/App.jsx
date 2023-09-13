e74061b753dd61cb97b31d26e17593c60abbc10f
gallone10 <enzo.gallone83@gmail.com>
2023-09-13 08:46:59 -0300

    Estilização Menu finalizada

--- aula-rotas/src/App.jsx
+++ aula-rotas/src/App.jsx
@@ -3,11 +3,14 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Menu from './components/menu'
+
 
 function App() {
   
   return (
     <>
+      <Menu/>
       <Outlet/>
     </>
   )
